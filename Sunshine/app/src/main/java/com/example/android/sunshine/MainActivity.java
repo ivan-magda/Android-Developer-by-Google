abe2311e66537adf7c910a5051311da8b87e74bc
Ivan Magda <imagda15@gmail.com>
2016-12-01 23:13:11 +0900

    [Sunshine] Show scrollable dummy weather data Create an array of a dummy weather data, iterate over it and append each value to the TextView.

--- Sunshine/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ Sunshine/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -17,12 +17,46 @@ package com.example.android.sunshine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mWeatherTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
+        configure();
+    }
+
+    private void configure() {
+        mWeatherTextView = (TextView) findViewById(R.id.tv_weather_data);
+
+        /*
+         * This String array contains dummy weather data. Later in the course, we're going to get
+         * real weather data. For now, we want to get something on the screen as quickly as
+         * possible, so we'll display this dummy data.
+         */
+        String[] dummyWeatherData = {
+                "Today, May 17 - Clear - 17°C / 15°C",
+                "Tomorrow - Cloudy - 19°C / 15°C",
+                "Thursday - Rainy- 30°C / 11°C",
+                "Friday - Thunderstorms - 21°C / 9°C",
+                "Saturday - Thunderstorms - 16°C / 7°C",
+                "Sunday - Rainy - 16°C / 8°C",
+                "Monday - Partly Cloudy - 15°C / 10°C",
+                "Tue, May 24 - Meatballs - 16°C / 18°C",
+                "Wed, May 25 - Cloudy - 19°C / 15°C",
+                "Thu, May 26 - Stormy - 30°C / 11°C",
+                "Fri, May 27 - Hurricane - 21°C / 9°C",
+                "Sat, May 28 - Meteors - 16°C / 7°C",
+                "Sun, May 29 - Apocalypse - 16°C / 8°C",
+                "Mon, May 30 - Post Apocalypse - 15°C / 10°C",
+        };
+
+        for (String dummyDay : dummyWeatherData) {
+            mWeatherTextView.append(dummyDay + "\n\n\n");
+        }
     }
 }
