867d6242283b995455ff3b6e8ea821799df8a523
Ivan Magda <imagda15@gmail.com>
2016-12-01 22:37:34 +0900

    [FavoriteToys] Show a list favorite toys Use FrameLayout with ScrollView as a child. Create model class ToyBox, that returns an array of favorite toys names.

--- FavoriteToys/app/src/main/java/com/ivanmagda/favoritetoys/MainActivity.java
+++ FavoriteToys/app/src/main/java/com/ivanmagda/favoritetoys/MainActivity.java
@@ -1,13 +1,26 @@
 package com.ivanmagda.favoritetoys;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mToysListTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        configure();
+    }
+
+    private void configure() {
+        mToysListTextView = (TextView) findViewById(R.id.tv_toy_names);
+
+        String[] names = ToyBox.getToyNames();
+        for (String name : names) {
+            mToysListTextView.append(name + "\n\n\n");
+        }
     }
 }
