909155cd2a9c4259a41f9ec403dc95a51266c892
Ivan Magda <imagda15@gmail.com>
2016-12-12 11:58:39 +0900

    chore: Add necessary URI's to the Contract
    Add the following constants:
    
    * Content authority
    * Base content URI
    * Path for tasks directory
    * Content URI for data in the TasksEntry class

--- ToDo-List/app/src/main/java/com/example/android/todolist/data/TaskContract.java
+++ ToDo-List/app/src/main/java/com/example/android/todolist/data/TaskContract.java
@@ -16,12 +16,29 @@
 
 package com.example.android.todolist.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public class TaskContract {
 
+    // The authority, which is how your code knows which Content Provider to access
+    public static final String AUTHORITY = "com.example.android.todolist";
+
+    // The base content URI = "content://" + <authority>
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
+
+    /**
+     * Define the possible paths for accessing data in this contract
+     * This is the path for the "tasks" directory.
+     */
+    public static final String PATH_TASKS = "tasks";
+
     /* TaskEntry is an inner class that defines the contents of the task table */
     public static final class TaskEntry implements BaseColumns {
+
+        // TaskEntry content URI = base content URI + path
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_TASKS).build();
 
         // Task table and column names
         public static final String TABLE_NAME = "tasks";
