abbd333ec6adae17b1bafcfc46856808147b6191
Ivan Magda <imagda15@gmail.com>
2016-12-13 00:22:19 +0900

    chore: Implement bulkInsert method
    Add a bunch of weather data, not just one.

--- Sunshine/app/src/main/java/com/example/android/sunshine/data/WeatherProvider.java
+++ Sunshine/app/src/main/java/com/example/android/sunshine/data/WeatherProvider.java
@@ -12,6 +12,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import com.example.android.sunshine.data.WeatherContract.WeatherEntry;
+import com.example.android.sunshine.utilities.SunshineDateUtils;
 
 public final class WeatherProvider extends ContentProvider {
 
@@ -145,34 +146,98 @@ public final class WeatherProvider extends ContentProvider {
         return cursor;
     }
 
+    /**
+     * In Sunshine, we aren't going to do anything with this method. However, we are required to
+     * override it as WeatherProvider extends ContentProvider and insert is an abstract method in
+     * ContentProvider. Rather than the single insert method, we are only going to implement
+     * {@link WeatherProvider#bulkInsert}.
+     *
+     * @param uri           The URI of the insertion request. This must not be null.
+     * @param contentValues A set of column_name/value pairs to add to the database.
+     *                      This must not be null
+     * @return nothing in Sunshine, but normally the URI for the newly inserted item.
+     */
     @Nullable
     @Override
     public Uri insert(@NonNull Uri uri, ContentValues contentValues) {
-        SQLiteDatabase database = mDatabaseHelper.getWritableDatabase();
-        switch (sUriMatcher.match(uri)) {
-            case WEATHER:
-                long rowId = database.insertOrThrow(WeatherEntry.TABLE_NAME, null, null);
+        throw new RuntimeException("We are not implementing insert in Sunshine. Use bulkInsert instead");
+    }
+
+    /**
+     * Handles requests to insert a set of new rows. In Sunshine, we are only going to be
+     * inserting multiple rows of data at a time from a weather forecast. There is no use case
+     * for inserting a single row of data into our ContentProvider, and so we are only going to
+     * implement bulkInsert. In a normal ContentProvider's implementation, you will probably want
+     * to provide proper functionality for the insert method as well.
+     *
+     * @param uri    The content:// URI of the insertion request.
+     * @param values An array of sets of column_name/value pairs to add to the database.
+     *               This must not be {@code null}.
+     * @return The number of values that were inserted.
+     */
+    @Override
+    public int bulkInsert(@NonNull Uri uri, @NonNull ContentValues[] values) {
+        final SQLiteDatabase database = mDatabaseHelper.getWritableDatabase();
+        int rowsInserted = 0;
+
+        if (sUriMatcher.match(uri) == WEATHER) {
+            database.beginTransaction();
+            try {
+                for (ContentValues value : values) {
+                    long weatherDate = value.getAsLong(WeatherEntry.COLUMN_DATE);
+                    if (!SunshineDateUtils.isDateNormalized(weatherDate)) {
+                        throw new IllegalArgumentException("Date must be normalized to insert");
+                    }
+                    long id = database.insertOrThrow(WeatherEntry.TABLE_NAME, null, value);
+                    if (id != -1) rowsInserted++;
+                }
+                database.setTransactionSuccessful();
+            } finally {
+                database.endTransaction();
+            }
+
+            if (rowsInserted > 0) {
                 notifyChangeForUri(uri);
-                return ContentUris.withAppendedId(uri, rowId);
-            default:
-                throw new UnsupportedOperationException("Unknown uri: " + uri);
+            }
+
+            return rowsInserted;
+        } else {
+            throw new UnsupportedOperationException("Unknown uri: " + uri);
         }
     }
 
+    /**
+     * Deletes data at a given URI with optional arguments for more fine tuned deletions.
+     *
+     * @param uri           The full URI to query
+     * @param selection     An optional restriction to apply to rows when deleting.
+     * @param selectionArgs Used in conjunction with the selection statement
+     * @return The number of rows deleted
+     */
     @Override
-    public int delete(@NonNull Uri uri, String s, String[] strings) {
-        return 0;
+    public int delete(@NonNull Uri uri, String selection, String[] selectionArgs) {
+        throw new RuntimeException("Method does't implemented");
     }
 
     @Override
     public int update(@NonNull Uri uri, ContentValues contentValues, String s, String[] strings) {
-        return 0;
+        throw new RuntimeException("We are not implementing update in Sunshine");
     }
 
+    /**
+     * In Sunshine, we aren't going to do anything with this method. However, we are required to
+     * override it as WeatherProvider extends ContentProvider and getType is an abstract method in
+     * ContentProvider. Normally, this method handles requests for the MIME type of the data at the
+     * given URI. For example, if your app provided images at a particular URI, then you would
+     * return an image URI from this method.
+     *
+     * @param uri the URI to query.
+     * @return nothing in Sunshine, but normally a MIME type string, or null if there is no type.
+     */
     @Nullable
     @Override
     public String getType(@NonNull Uri uri) {
-        return null;
+        throw new RuntimeException("We are not implementing getType in Sunshine.");
     }
 
     private void notifyChangeForUri(Uri uri) {
