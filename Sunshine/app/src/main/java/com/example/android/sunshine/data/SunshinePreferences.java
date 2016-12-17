01e3c5a8f0c2960ab2194561f190f6f336c13025
Ivan Magda <imagda15@gmail.com>
2016-12-17 20:24:07 +0900

    feat: Forecast notifications

--- Sunshine/app/src/main/java/com/example/android/sunshine/data/SunshinePreferences.java
+++ Sunshine/app/src/main/java/com/example/android/sunshine/data/SunshinePreferences.java
@@ -18,6 +18,7 @@ package com.example.android.sunshine.data;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 
 import com.example.android.sunshine.R;
 
@@ -212,4 +213,32 @@ public final class SunshinePreferences {
         editor.putLong(lastNotificationKey, timeOfNotification);
         editor.apply();
     }
+
+    /**
+     * Returns true if the user prefers to see notifications from Sunshine, false otherwise. This
+     * preference can be changed by the user within the SettingsFragment.
+     *
+     * @param context Used to access SharedPreferences
+     * @return true if the user prefers to see notifications, false otherwise
+     */
+    public static boolean areNotificationsEnabled(@NonNull final Context context) {
+        /* Key for accessing the preference for showing notifications */
+        String displayNotificationsKey = context.getString(R.string.pref_enable_notifications_key);
+
+        /*
+         * In Sunshine, the user has the ability to say whether she would like notifications
+         * enabled or not. If no preference has been chosen, we want to be able to determine
+         * whether or not to show them. To do this, we reference a bool stored in bools.xml.
+         */
+        boolean shouldDisplayNotificationsByDefault = context
+                .getResources()
+                .getBoolean(R.bool.show_notifications_by_default);
+
+        /* As usual, we use the default SharedPreferences to access the user's preferences */
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
+
+        /* If a value is stored with the key, we extract it here. If not, use a default. */
+        return sp.getBoolean(displayNotificationsKey, shouldDisplayNotificationsByDefault);
+    }
+
 }
--- Sunshine/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
+++ Sunshine/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
@@ -18,9 +18,12 @@ package com.example.android.sunshine.sync;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
+import android.text.format.DateUtils;
 
+import com.example.android.sunshine.data.SunshinePreferences;
 import com.example.android.sunshine.data.WeatherContract;
 import com.example.android.sunshine.utilities.NetworkUtils;
+import com.example.android.sunshine.utilities.NotificationUtils;
 import com.example.android.sunshine.utilities.OpenWeatherJsonUtils;
 
 import java.net.URL;
@@ -59,7 +62,7 @@ public class SunshineSyncTask {
              * there isn't any to insert.
              */
             if (weatherValues != null && weatherValues.length != 0) {
-                /* Get a handle on the ContentResolver to delete and insert data */
+                /** Get a handle on the ContentResolver to delete and insert data */
                 ContentResolver sunshineContentResolver = context.getContentResolver();
 
                 /** Delete old weather data because we don't need to keep multiple days' data */
@@ -68,14 +71,40 @@ public class SunshineSyncTask {
                         null,
                         null);
 
-                /* Insert our new weather data into Sunshine's ContentProvider */
+                /** Insert our new weather data into Sunshine's ContentProvider */
                 sunshineContentResolver.bulkInsert(
                         WeatherContract.WeatherEntry.CONTENT_URI,
                         weatherValues);
+
+                /**
+                 * Finally, after we insert data into the ContentProvider, determine whether or not
+                 * we should notify the user that the weather has been refreshed.
+                 */
+                boolean notificationsEnabled = SunshinePreferences.areNotificationsEnabled(context);
+
+                /**
+                 * If the last notification was shown was more than 1 day ago, we want to send
+                 * another notification to the user that the weather has been updated. Remember,
+                 * it's important that you shouldn't spam your users with notifications.
+                 */
+                long timeSinceLastNotification = SunshinePreferences
+                        .getEllapsedTimeSinceLastNotification(context);
+
+                boolean oneDayPassedSinceLastNotification = false;
+
+                if (timeSinceLastNotification >= DateUtils.DAY_IN_MILLIS) {
+                    oneDayPassedSinceLastNotification = true;
+                }
+
+                /**
+                 * We only want to show the notification if the user wants them shown and we
+                 * haven't shown a notification in the past day.
+                 */
+                if (notificationsEnabled && oneDayPassedSinceLastNotification) {
+                    NotificationUtils.notifyUserOfNewWeather(context);
+                }
+
             }
-
-            /* If the code reaches this point, we have successfully performed our sync */
-
         } catch (Exception e) {
             /* Server probably invalid */
             e.printStackTrace();
