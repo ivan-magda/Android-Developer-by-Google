450ed5c444e00e57ed1b538fcdbc270aed1c2666
Ivan Magda <imagda15@gmail.com>
2016-12-18 14:43:10 +0900

    feat: Create landscape layout

--- BoardingPass/app/src/main/java/com/example/android/boardingpass/MainActivity.java
+++ BoardingPass/app/src/main/java/com/example/android/boardingpass/MainActivity.java
@@ -49,9 +49,9 @@ public class MainActivity extends AppCompatActivity {
 
     private void displayBoardingPassInfo(BoardingPassInfo info) {
         mBinding.textViewPassengerName.setText(info.passengerName);
-        mBinding.textViewOriginAirport.setText(info.originCode);
-        mBinding.textViewFlightCode.setText(info.flightCode);
-        mBinding.textViewDestinationAirport.setText(info.destCode);
+        mBinding.flightInfo.textViewOriginAirport.setText(info.originCode);
+        mBinding.flightInfo.textViewFlightCode.setText(info.flightCode);
+        mBinding.flightInfo.textViewDestinationAirport.setText(info.destCode);
 
         SimpleDateFormat formatter = new SimpleDateFormat(getString(R.string.timeFormat), Locale.getDefault());
         String boardingTime = formatter.format(info.boardingTime);
@@ -72,9 +72,9 @@ public class MainActivity extends AppCompatActivity {
                 minutesLessHoursUntilBoarding);
 
         mBinding.textViewBoardingInCountdown.setText(hoursAndMinutesUntilBoarding);
-        mBinding.textViewTerminal.setText(info.departureTerminal);
-        mBinding.textViewGate.setText(info.departureGate);
-        mBinding.textViewSeat.setText(info.seatNumber);
+        mBinding.boardingInfo.textViewTerminal.setText(info.departureTerminal);
+        mBinding.boardingInfo.textViewGate.setText(info.departureGate);
+        mBinding.boardingInfo.textViewSeat.setText(info.seatNumber);
     }
 
 }
