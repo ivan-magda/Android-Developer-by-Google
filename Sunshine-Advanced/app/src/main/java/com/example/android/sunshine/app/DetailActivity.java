24ebf75ccdd1d2f14ceefba3f78f6ea77f56464a
Ivan Magda <imagda15@gmail.com>
2017-01-06 15:10:45 +0900

    feat: Update detail view

--- Sunshine-Advanced/app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
+++ Sunshine-Advanced/app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -15,12 +15,8 @@
  */
 package com.example.android.sunshine.app;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class DetailActivity extends AppCompatActivity {
@@ -29,10 +25,6 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         if (savedInstanceState == null) {
             // Create the detail fragment and add it to the activity
@@ -48,25 +40,5 @@ public class DetailActivity extends AppCompatActivity {
                     .add(R.id.weather_detail_container, fragment)
                     .commit();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.detail, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            startActivity(new Intent(this, SettingsActivity.class));
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
--- Sunshine-Advanced/app/src/main/java/com/example/android/sunshine/app/Utility.java
+++ Sunshine-Advanced/app/src/main/java/com/example/android/sunshine/app/Utility.java
@@ -15,13 +15,11 @@
  */
 package com.example.android.sunshine.app;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.preference.PreferenceManager;
-import android.support.annotation.NonNull;
 import android.text.format.Time;
 
 import com.example.android.sunshine.app.sync.SunshineSyncAdapter;
@@ -32,35 +30,6 @@ import java.util.Date;
 import java.util.Locale;
 
 public class Utility {
-
-    public static boolean isNetworkAvailable(@NonNull final Context context) {
-        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
-        return networkInfo != null && networkInfo.isConnectedOrConnecting();
-    }
-
-    @SuppressWarnings("ResourceType")
-    public static
-    @SunshineSyncAdapter.LocationStatus
-    int getLocationStatus(final Context context) {
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-        return sharedPreferences.getInt(context.getString(R.string.pref_location_status_key),
-                SunshineSyncAdapter.LOCATION_STATUS_UNKNOWN);
-    }
-
-    /**
-     * Resets the location status.
-     *
-     * @param context Context used to get the SharedPreferences
-     */
-    public static void resetLocationStatus(final Context context) {
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putInt(context.getString(R.string.pref_location_status_key),
-                SunshineSyncAdapter.LOCATION_STATUS_UNKNOWN);
-        editor.apply();
-    }
-
     public static String getPreferredLocation(Context context) {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
         return prefs.getString(context.getString(R.string.pref_location_key),
@@ -103,7 +72,6 @@ public class Utility {
      * @param dateInMillis The date in milliseconds
      * @return a user-friendly representation of the date.
      */
-    @SuppressLint("StringFormatMatches")
     public static String getFriendlyDayString(Context context, long dateInMillis) {
         // The day string for forecast uses the following logic:
         // For today: "Today, June 8"
@@ -134,6 +102,24 @@ public class Utility {
             SimpleDateFormat shortenedDateFormat = new SimpleDateFormat("EEE MMM dd");
             return shortenedDateFormat.format(dateInMillis);
         }
+    }
+
+    /**
+     * Helper method to convert the database representation of the date into something to display
+     * to users.  As classy and polished a user experience as "20140102" is, we can do better.
+     *
+     * @param context      Context to use for resource localization
+     * @param dateInMillis The date in milliseconds
+     * @return a user-friendly representation of the date.
+     */
+    public static String getFullFriendlyDayString(Context context, long dateInMillis) {
+
+        String day = getDayName(context, dateInMillis);
+        int formatId = R.string.format_full_friendly_date;
+        return String.format(context.getString(
+                formatId,
+                day,
+                getFormattedMonthDay(context, dateInMillis)));
     }
 
     /**
@@ -252,39 +238,16 @@ public class Utility {
     }
 
     /**
-     * Helper method to provide the art resource id according to the weather condition id returned
-     * by the OpenWeatherMap call.
+     * Helper method to return whether or not Sunshine is using local graphics.
      *
-     * @param weatherId from OpenWeatherMap API response
-     * @return resource id for the corresponding icon. -1 if no relation is found.
+     * @param context Context to use for retrieving the preference
+     * @return true if Sunshine is using local graphics, false otherwise.
      */
-    public static int getArtResourceForWeatherCondition(int weatherId) {
-        // Based on weather code data found at:
-        // http://bugs.openweathermap.org/projects/api/wiki/Weather_Condition_Codes
-        if (weatherId >= 200 && weatherId <= 232) {
-            return R.drawable.art_storm;
-        } else if (weatherId >= 300 && weatherId <= 321) {
-            return R.drawable.art_light_rain;
-        } else if (weatherId >= 500 && weatherId <= 504) {
-            return R.drawable.art_rain;
-        } else if (weatherId == 511) {
-            return R.drawable.art_snow;
-        } else if (weatherId >= 520 && weatherId <= 531) {
-            return R.drawable.art_rain;
-        } else if (weatherId >= 600 && weatherId <= 622) {
-            return R.drawable.art_snow;
-        } else if (weatherId >= 701 && weatherId <= 761) {
-            return R.drawable.art_fog;
-        } else if (weatherId == 761 || weatherId == 781) {
-            return R.drawable.art_storm;
-        } else if (weatherId == 800) {
-            return R.drawable.art_clear;
-        } else if (weatherId == 801) {
-            return R.drawable.art_light_clouds;
-        } else if (weatherId >= 802 && weatherId <= 804) {
-            return R.drawable.art_clouds;
-        }
-        return -1;
+    public static boolean usingLocalGraphics(Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        String sunshineArtPack = context.getString(R.string.pref_art_pack_sunshine);
+        return prefs.getString(context.getString(R.string.pref_art_pack_key),
+                sunshineArtPack).equals(sunshineArtPack);
     }
 
     /**
@@ -328,4 +291,257 @@ public class Utility {
         return null;
     }
 
+    /**
+     * Helper method to provide the art resource id according to the weather condition id returned
+     * by the OpenWeatherMap call.
+     *
+     * @param weatherId from OpenWeatherMap API response
+     * @return resource id for the corresponding icon. -1 if no relation is found.
+     */
+    public static int getArtResourceForWeatherCondition(int weatherId) {
+        // Based on weather code data found at:
+        // http://bugs.openweathermap.org/projects/api/wiki/Weather_Condition_Codes
+        if (weatherId >= 200 && weatherId <= 232) {
+            return R.drawable.art_storm;
+        } else if (weatherId >= 300 && weatherId <= 321) {
+            return R.drawable.art_light_rain;
+        } else if (weatherId >= 500 && weatherId <= 504) {
+            return R.drawable.art_rain;
+        } else if (weatherId == 511) {
+            return R.drawable.art_snow;
+        } else if (weatherId >= 520 && weatherId <= 531) {
+            return R.drawable.art_rain;
+        } else if (weatherId >= 600 && weatherId <= 622) {
+            return R.drawable.art_snow;
+        } else if (weatherId >= 701 && weatherId <= 761) {
+            return R.drawable.art_fog;
+        } else if (weatherId == 761 || weatherId == 781) {
+            return R.drawable.art_storm;
+        } else if (weatherId == 800) {
+            return R.drawable.art_clear;
+        } else if (weatherId == 801) {
+            return R.drawable.art_light_clouds;
+        } else if (weatherId >= 802 && weatherId <= 804) {
+            return R.drawable.art_clouds;
+        }
+        return -1;
+    }
+
+    /**
+     * Helper method to provide the string according to the weather
+     * condition id returned by the OpenWeatherMap call.
+     *
+     * @param context   Android context
+     * @param weatherId from OpenWeatherMap API response
+     * @return string for the weather condition. null if no relation is found.
+     */
+    public static String getStringForWeatherCondition(Context context, int weatherId) {
+        // Based on weather code data found at:
+        // http://bugs.openweathermap.org/projects/api/wiki/Weather_Condition_Codes
+        int stringId;
+        if (weatherId >= 200 && weatherId <= 232) {
+            stringId = R.string.condition_2xx;
+        } else if (weatherId >= 300 && weatherId <= 321) {
+            stringId = R.string.condition_3xx;
+        } else switch (weatherId) {
+            case 500:
+                stringId = R.string.condition_500;
+                break;
+            case 501:
+                stringId = R.string.condition_501;
+                break;
+            case 502:
+                stringId = R.string.condition_502;
+                break;
+            case 503:
+                stringId = R.string.condition_503;
+                break;
+            case 504:
+                stringId = R.string.condition_504;
+                break;
+            case 511:
+                stringId = R.string.condition_511;
+                break;
+            case 520:
+                stringId = R.string.condition_520;
+                break;
+            case 531:
+                stringId = R.string.condition_531;
+                break;
+            case 600:
+                stringId = R.string.condition_600;
+                break;
+            case 601:
+                stringId = R.string.condition_601;
+                break;
+            case 602:
+                stringId = R.string.condition_602;
+                break;
+            case 611:
+                stringId = R.string.condition_611;
+                break;
+            case 612:
+                stringId = R.string.condition_612;
+                break;
+            case 615:
+                stringId = R.string.condition_615;
+                break;
+            case 616:
+                stringId = R.string.condition_616;
+                break;
+            case 620:
+                stringId = R.string.condition_620;
+                break;
+            case 621:
+                stringId = R.string.condition_621;
+                break;
+            case 622:
+                stringId = R.string.condition_622;
+                break;
+            case 701:
+                stringId = R.string.condition_701;
+                break;
+            case 711:
+                stringId = R.string.condition_711;
+                break;
+            case 721:
+                stringId = R.string.condition_721;
+                break;
+            case 731:
+                stringId = R.string.condition_731;
+                break;
+            case 741:
+                stringId = R.string.condition_741;
+                break;
+            case 751:
+                stringId = R.string.condition_751;
+                break;
+            case 761:
+                stringId = R.string.condition_761;
+                break;
+            case 762:
+                stringId = R.string.condition_762;
+                break;
+            case 771:
+                stringId = R.string.condition_771;
+                break;
+            case 781:
+                stringId = R.string.condition_781;
+                break;
+            case 800:
+                stringId = R.string.condition_800;
+                break;
+            case 801:
+                stringId = R.string.condition_801;
+                break;
+            case 802:
+                stringId = R.string.condition_802;
+                break;
+            case 803:
+                stringId = R.string.condition_803;
+                break;
+            case 804:
+                stringId = R.string.condition_804;
+                break;
+            case 900:
+                stringId = R.string.condition_900;
+                break;
+            case 901:
+                stringId = R.string.condition_901;
+                break;
+            case 902:
+                stringId = R.string.condition_902;
+                break;
+            case 903:
+                stringId = R.string.condition_903;
+                break;
+            case 904:
+                stringId = R.string.condition_904;
+                break;
+            case 905:
+                stringId = R.string.condition_905;
+                break;
+            case 906:
+                stringId = R.string.condition_906;
+                break;
+            case 951:
+                stringId = R.string.condition_951;
+                break;
+            case 952:
+                stringId = R.string.condition_952;
+                break;
+            case 953:
+                stringId = R.string.condition_953;
+                break;
+            case 954:
+                stringId = R.string.condition_954;
+                break;
+            case 955:
+                stringId = R.string.condition_955;
+                break;
+            case 956:
+                stringId = R.string.condition_956;
+                break;
+            case 957:
+                stringId = R.string.condition_957;
+                break;
+            case 958:
+                stringId = R.string.condition_958;
+                break;
+            case 959:
+                stringId = R.string.condition_959;
+                break;
+            case 960:
+                stringId = R.string.condition_960;
+                break;
+            case 961:
+                stringId = R.string.condition_961;
+                break;
+            case 962:
+                stringId = R.string.condition_962;
+                break;
+            default:
+                return context.getString(R.string.condition_unknown, weatherId);
+        }
+        return context.getString(stringId);
+    }
+
+    /**
+     * Returns true if the network is available or about to become available.
+     *
+     * @param c Context used to get the ConnectivityManager
+     * @return true if the network is available
+     */
+    static public boolean isNetworkAvailable(Context c) {
+        ConnectivityManager cm =
+                (ConnectivityManager) c.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+    }
+
+    /**
+     * @param c Context used to get the SharedPreferences
+     * @return the location status integer type
+     */
+    @SuppressWarnings("ResourceType")
+    static public
+    @SunshineSyncAdapter.LocationStatus
+    int getLocationStatus(Context c) {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(c);
+        return sp.getInt(c.getString(R.string.pref_location_status_key), SunshineSyncAdapter.LOCATION_STATUS_UNKNOWN);
+    }
+
+    /**
+     * Resets the location status.  (Sets it to SunshineSyncAdapter.LOCATION_STATUS_UNKNOWN)
+     *
+     * @param c Context used to get the SharedPreferences
+     */
+    static public void resetLocationStatus(Context c) {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(c);
+        SharedPreferences.Editor spe = sp.edit();
+        spe.putInt(c.getString(R.string.pref_location_status_key), SunshineSyncAdapter.LOCATION_STATUS_UNKNOWN);
+        spe.apply();
+    }
 }
