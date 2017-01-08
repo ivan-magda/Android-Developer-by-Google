5bb13fa3b8ab9af18784abacada267bf2b37c401
Ivan Magda <imagda15@gmail.com>
2017-01-08 16:46:00 +0900

    feat: Create layout for current location

--- Sunshine-Advanced/app/src/main/java/com/example/android/sunshine/app/LocationEditTextPreference.java
+++ Sunshine-Advanced/app/src/main/java/com/example/android/sunshine/app/LocationEditTextPreference.java
@@ -9,8 +9,14 @@ import android.preference.EditTextPreference;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 public class LocationEditTextPreference extends EditTextPreference {
 
@@ -29,6 +35,26 @@ public class LocationEditTextPreference extends EditTextPreference {
         } finally {
             array.recycle();
         }
+
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int resultCode = apiAvailability.isGooglePlayServicesAvailable(getContext());
+        if (resultCode == ConnectionResult.SUCCESS) {
+            setWidgetLayoutResource(R.layout.pref_current_location);
+        }
+    }
+
+    @Override
+    protected View onCreateView(ViewGroup parent) {
+        View view = super.onCreateView(parent);
+        View currentLocation = view.findViewById(R.id.current_location);
+        currentLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Woo!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
     }
 
     @Override
